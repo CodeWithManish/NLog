9c0388c69f23d6b2aa3750b9c6bfc39067c9b3da
Manish <manishkumar0156@gmail.com>
2022-02-02 16:33:28 +0530

    [Manish] Add: upload nlog project

---
+++ NLogProject/NLogProject/Example.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogProject
+{
+    public class Example
+    {
+        Nlog nlog = new Nlog();
+        public void Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                nlog.LogInfo("Sum()");
+                nlog.LogDebug("Debug Method sum()");
+                nlog.LogError("Error occurred in some method");
+
+            }
+            int c = a + b;
+            nlog.LogInfo("Sum()");
+            nlog.LogDebug("Debug Method sum()");
+            
+        }
+    }
+}
---
+++ NLogProject/NLogProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Example example = new Example();
+            example.Sum(4,2);
+        }
+    }
+}
